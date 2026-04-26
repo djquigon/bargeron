23dbf8c6f84e544c8854e444bbbc629a45db3cad
Logan Scheid <scheid.logan@gmail.com>
2026-04-25 22:13:36 -0400

    Refactor footer layout and styling for improved consistency and functionality. Updated footer.php to enhance semantic structure and added new SCSS styles for footer components, including quicklinks and contact info. Adjusted spacing variables and improved logo references in site logo widgets. Enhanced heading styles for better readability.

--- wp-content/themes/mrmastertheme/views/global/footer/navigation/quicklinks/quicklinks.php
+++ wp-content/themes/mrmastertheme/views/global/footer/navigation/quicklinks/quicklinks.php
@@ -1,17 +1,17 @@
 <?php
-    //It's pretty common for a Footer to include a short 'Quicklinks' menu
+//It's pretty common for a Footer to include a short 'Quicklinks' menu
 
-    //We have a dedicated menu location, 'footer_menu', for this widget
+//We have a dedicated menu location, 'footer_menu', for this widget
 ?>
-<h2 class="h4">Quicklinks</h2>
+<h4>Quick Links</h4>
 <?php
-    if (has_nav_menu('footer_menu')) {
-        wp_nav_menu(array(
-            'container'       => 'ul',
-            'menu_class'      => 'quicklinks',
-            'menu_id'         => '',
-            'depth'           => 0,
-            'theme_location' => 'footer_menu'
-        ));
-    }
+if (has_nav_menu('footer_menu')) {
+    wp_nav_menu(array(
+        'container'       => 'ul',
+        'menu_class'      => 'quicklinks',
+        'menu_id'         => '',
+        'depth'           => 0,
+        'theme_location' => 'footer_menu'
+    ));
+}
 ?>
