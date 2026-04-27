a4936b6a4c289e55341fe31f7a961e12f9b1c333
Logan Scheid <scheid.logan@gmail.com>
2026-04-27 16:22:48 -0400

    Refactor SCSS styles for standard content layout and toggle components. Updated padding and background settings for improved visual appeal and user interaction. Enhanced column layouts with responsive adjustments and refined icon styles for better alignment. Improved accessibility features across various components.

--- wp-content/themes/mrmastertheme/views/global/modules/standard-content/layout-options/four-column.php
+++ wp-content/themes/mrmastertheme/views/global/modules/standard-content/layout-options/four-column.php
@@ -1,63 +1,62 @@
 <?php
-    //all the data for THIS row
-    $content_row = $args['content_row'];
+//all the data for THIS row
+$content_row = $args['content_row'];
+$row_class = $args['row_class'];
+//colors are set in the module's settings
+$text_color_attribute = $args['text_color_attribute'];
 
-    //colors are set in the module's settings
-    $text_color_attribute = $args['text_color_attribute'];
+//grab the settings for this row
+if (isset($content_row['container_width'])) {
+    $container_width = $content_row['container_width'];
+} else {
+    $container_width = false;
+}
 
-    //grab the settings for this row
-    if (isset($content_row['container_width'])) {
-        $container_width = $content_row['container_width'];
-    } else {
-        $container_width = false;
-    }
+if (isset($content_row['include_lr_padding'])) {
+    $widest_container_padding = $content_row['include_lr_padding'];
+} else {
+    $widest_container_padding = false;
+}
 
-    if (isset($content_row['include_lr_padding'])) {
-        $widest_container_padding = $content_row['include_lr_padding'];
-    } else {
-        $widest_container_padding = false;
-    }
-    
-    $column_count = $content_row['column_count'];
+$column_count = $content_row['column_count'];
 
-    //grab the content for each column
-    $column_content_fourths_one = $content_row['column_content_fourths_one'];
-    $column_content_fourths_two = $content_row['column_content_fourths_two'];
-    $column_content_fourths_three = $content_row['column_content_fourths_three'];
-    $column_content_fourths_four = $content_row['column_content_fourths_four'];
+//grab the content for each column
+$column_content_fourths_one = $content_row['column_content_fourths_one'];
+$column_content_fourths_two = $content_row['column_content_fourths_two'];
+$column_content_fourths_three = $content_row['column_content_fourths_three'];
+$column_content_fourths_four = $content_row['column_content_fourths_four'];
 ?>
-<div class="content-row">
+<div class="content-row <?= $row_class ?>">
     <div class="columns">
-        <div class="column one"<?= $text_color_attribute ?>>
+        <div class="column one" <?= $text_color_attribute ?>>
             <?= $column_content_fourths_one ?>
         </div>
-        <div class="column two"<?= $text_color_attribute ?>>
+        <div class="column two" <?= $text_color_attribute ?>>
             <?= $column_content_fourths_two ?>
         </div>
-        <div class="column three"<?= $text_color_attribute ?>>
+        <div class="column three" <?= $text_color_attribute ?>>
             <?= $column_content_fourths_three ?>
         </div>
-        <div class="column four"<?= $text_color_attribute ?>>
+        <div class="column four" <?= $text_color_attribute ?>>
             <?= $column_content_fourths_four ?>
         </div>
     </div>
-    <span 
-        class="row-settings" 
-        data-column-count="<?= $column_count ?>" 
+    <span
+        class="row-settings"
+        data-column-count="<?= $column_count ?>"
         <?php
-            if ($container_width) :
+        if ($container_width) :
         ?>
-                data-container-width="<?= $container_width ?>"
+        data-container-width="<?= $container_width ?>"
         <?php
-            endif;
-        
-            if ($widest_container_padding) : 
-        ?> 
-                data-container-padding="true"
-        <?php 
-            endif; 
+        endif;
+
+        if ($widest_container_padding) :
         ?>
-    >
+        data-container-padding="true"
+        <?php
+        endif;
+        ?>>
         <span class="validator-text" data-nosnippet>row settings</span>
     </span>
 </div>
--- wp-content/themes/mrmastertheme/views/global/modules/standard-content/layout-options/three-column.php
+++ wp-content/themes/mrmastertheme/views/global/modules/standard-content/layout-options/three-column.php
@@ -1,59 +1,58 @@
 <?php
-    //all the data for THIS row
-    $content_row = $args['content_row'];
+//all the data for THIS row
+$content_row = $args['content_row'];
+$row_class = $args['row_class'];
+//colors are set in the module's settings
+$text_color_attribute = $args['text_color_attribute'];
 
-    //colors are set in the module's settings
-    $text_color_attribute = $args['text_color_attribute'];
+//grab the settings for this row
+if (isset($content_row['container_width'])) {
+    $container_width = $content_row['container_width'];
+} else {
+    $container_width = false;
+}
 
-    //grab the settings for this row
-    if (isset($content_row['container_width'])) {
-        $container_width = $content_row['container_width'];
-    } else {
-        $container_width = false;
-    }
+if (isset($content_row['include_lr_padding'])) {
+    $widest_container_padding = $content_row['include_lr_padding'];
+} else {
+    $widest_container_padding = false;
+}
 
-    if (isset($content_row['include_lr_padding'])) {
-        $widest_container_padding = $content_row['include_lr_padding'];
-    } else {
-        $widest_container_padding = false;
-    }
-    
-    $column_count = $content_row['column_count'];
+$column_count = $content_row['column_count'];
 
-    //grab the content for each column
-    $column_content_left = $content_row['column_content_thirds_left'];
-    $column_content_center = $content_row['column_content_thirds_center'];
-    $column_content_right = $content_row['column_content_thirds_right'];
+//grab the content for each column
+$column_content_left = $content_row['column_content_thirds_left'];
+$column_content_center = $content_row['column_content_thirds_center'];
+$column_content_right = $content_row['column_content_thirds_right'];
 ?>
-<div class="content-row">
+<div class="content-row <?= $row_class ?>">
     <div class="columns">
-        <div class="column left"<?= $text_color_attribute ?>>
+        <div class="column left" <?= $text_color_attribute ?>>
             <?= $column_content_left ?>
         </div>
-        <div class="column center"<?= $text_color_attribute ?>>
+        <div class="column center" <?= $text_color_attribute ?>>
             <?= $column_content_center ?>
         </div>
-        <div class="column right"<?= $text_color_attribute ?>>
+        <div class="column right" <?= $text_color_attribute ?>>
             <?= $column_content_right ?>
         </div>
     </div>
-    <span 
-        class="row-settings" 
-        data-column-count="<?= $column_count ?>" 
+    <span
+        class="row-settings"
+        data-column-count="<?= $column_count ?>"
         <?php
-            if ($container_width) :
+        if ($container_width) :
         ?>
-                data-container-width="<?= $container_width ?>"
+        data-container-width="<?= $container_width ?>"
         <?php
-            endif;
-        
-            if ($widest_container_padding) : 
-        ?> 
-                data-container-padding="true"
-        <?php 
-            endif; 
+        endif;
+
+        if ($widest_container_padding) :
         ?>
-    >
+        data-container-padding="true"
+        <?php
+        endif;
+        ?>>
         <span class="validator-text" data-nosnippet>row settings</span>
     </span>
 </div>
